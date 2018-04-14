76f86ab1e6b101bc4544dc9a82e0db040805a78e
Xxdstem <xxdstem@yandex.ru>
2018-04-15 00:39:58 +0300

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="footer-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="footer-info-block">
+                                <a class="footer-title">Помощь</a>
+                                <ul>
+                                  <li>Как начать игру</li>
+                                  <li>Лаунчер</li>
+                                  <li>Гайды от диггера</li>
+                                  <li>Кулсторибоб</li>
+                                </ul>
+                            </div>
+                             <div class="footer-info-block">
+                                <a class="footer-title">Помощь</a>
+                                <ul>
+                                  <li>Как начать игру</li>
+                                  <li>Лаунчер</li>
+                                  <li>Гайды от диггера</li>
+                                  <li>Кулсторибоб</li>
+                                </ul>
+                            </div>
+                             <div class="footer-info-block">
+                                <a class="footer-title">Помощь</a>
+                                <ul>
+                                  <li>Как начать игру</li>
+                                  <li>Лаунчер</li>
+                                  <li>Гайды от диггера</li>
+                                  <li>Кулсторибоб</li>
+                                </ul>
+                            </div>
+                             <div class="footer-logo">
+                                 osu!gatari
+                                 <br>
+                                 <a style="font-size: 18px; padding-right: 6px"> приватный osu! сервер</a>
+                            </div>
+            
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBlock'
+}
+</script>
