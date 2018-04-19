54cff95d4bbc2df9bc4c69c10164dab5b4dae0f7
Halfy44751 <shokocompany@gmail.com>
2018-04-19 18:25:55 +0300

    beatmap 1ver

--- src/pages/Beatmaps.vue
+++ src/pages/Beatmaps.vue
@@ -1,6 +1,35 @@
 <template>
+
     <div id="beatmaps">
-       
+            <link rel="stylesheet" href="css/bm-main.css">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                <div class="beatmap-banner">
+                    <div class="beatmap-banner-stroke block-pad">Beatmaps</div>
+                    <div class="beatmap-banner-stroke2 block-pad">Here you can find any map what you want! <br>
+                        Feel free to download it from our server!
+
+                    </div>
+
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="beatmap-select-bg">
+                       <div class="col-lg-6">
+                           <div class="beatmap-block">
+                               
+                           </div>
+                       </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        
 
 
 
