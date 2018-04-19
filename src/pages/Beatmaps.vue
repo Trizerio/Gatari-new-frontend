05e4767432565dd14fdaf50081ae676e2342a144
Xxdstem <xxdstem@yandex.ru>
2018-04-19 18:32:51 +0300

    beatmap card

--- src/pages/Beatmaps.vue
+++ src/pages/Beatmaps.vue
@@ -18,15 +18,35 @@
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="beatmap-select-bg">
-                       <div class="col-lg-6">
-                           <div class="beatmap-block">
-                               
-                           </div>
-                       </div> 
+                <div class="col-sm-6">
+                    <audio id="audio_709296" src="https://b.ppy.sh/preview/709296.mp3"></audio>
+
+                            <div class="card hovercard">
+                        <a target="_blank" href="https://osu.gatari.pw/s/709296" class="cardheader ranked" id="709296" style="background: url('https://assets.ppy.sh/beatmaps/709296/covers/cover.jpg');">
+                        <span class="beatmap songname title">Take This Life</span>
+                        <span class="beatmap songname">In Flames</span>
+
+                        </a>
+                        
+                        
+                        <div class="info">
+                            <div class="play-download">
+                                
+                                <a class="btn-orange btn btn-circle  btn-sm" target="_blank" href="https://osu.gatari.pw/d/709296"><i class="fa fa-download"></i></a>
+                            </div>
+
+                            <div class="desc">
+                                Mapped by LMT
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            
+                            <a href="#" data-toggle="popover" data-placement="right" data-content="kiry's Tragic Love Expert" data-trigger="hover" data-original-title="" title=""><div class="diffIcon expert"></div></a><a href="#" data-toggle="popover" data-placement="right" data-content="End Me" data-trigger="hover" data-original-title="" title=""><div class="diffIcon expert"></div></a>
+                        </div>
                     </div>
-                </div>
+                <div class="beatmapset-panel__shadow"></div>
+                <span class="beatmapset-panel-prev beatmapset-panel__play js-audio--play">
+                <span onclick="play(709296);" id="icon_709296" class="fa fa-play"></span></span></div>
             </div>
         </div>
         
