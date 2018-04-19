2592a2726cb79d75b18245a5bcd4e736475f2940
Xxdstem <xxdstem@yandex.ru>
2018-04-19 20:44:03 +0300

    Beatmap block

--- src/pages/Beatmaps.vue
+++ src/pages/Beatmaps.vue
@@ -21,48 +21,7 @@
             <div class="row">
             <div class="col-lg-12">
             <div class="beatmap-select-bg">
-                <div class="col-sm-6">
-                    <audio id="audio_709296" src="https://b.ppy.sh/preview/709296.mp3"></audio>
-
-                            <div class="card hovercard">
-                        <a target="_blank" href="https://osu.gatari.pw/s/709296" class="cardheader ranked" id="709296" 
-                        style="background: url('https://assets.ppy.sh/beatmaps/709296/covers/cover.jpg');">
-                        <span class="beatmap songname title">Take This Life</span>
-                        <span class="beatmap songname">In Flames</span>
-
-                            <div class="song-fav">
-                                    45546 <i class="fas fa-heart"></i>
-                            </div>
-                        </a>
-                        <div class="song-status">
-                           RANKED
-                        </div>
-              
-                        
-                        <div class="info">
-                            <div class="play-download">
-                                
-                                <a class="download-icon" target="_blank" href="https://osu.gatari.pw/d/709296"><i class="fa fa-download"></i></a>
-                            </div>
-
-                            <div class="desc">
-                                    автор <a class="song-author-name"> authorname</a>
-                            </div>
-                            <div class="song-source">
-                                    SOUND VOLTEX III GRAVITY WARS
-                            </div>
-                        </div>
-                        <div class="bottom">
-                            
-                            <a href="#" data-toggle="popover" data-placement="right" data-content="kiry's Tragic Love Expert" 
-                            data-trigger="hover" data-original-title="" title=""><div class="diffIcon expert"></div></a><a href="#"
-                             data-toggle="popover" data-placement="right" data-content="End Me" data-trigger="hover" data-original-title="" title="">
-                             <div class="diffIcon expert"></div></a>
-                        </div>
-                    </div>
-               <!-- <div class="beatmapset-panel__shadow"></div> -->
-                <span class="beatmapset-panel-prev beatmapset-panel__play js-audio--play">
-                <span onclick="play(709296);" id="icon_709296" class="fa fa-play"></span></span></div>
+                <beatmap/>
                 </div>
             </div>
             </div>
@@ -71,8 +30,12 @@
 </template>
 
 <script>
+    import beatmap from '@/components/BeatmapsList/Beatmap.vue'
     export default {
-    name: 'Beatmaps'
-}
+    name: 'Beatmaps',
+    components:{
+        beatmap
+    }
+    }
 
 </script>
